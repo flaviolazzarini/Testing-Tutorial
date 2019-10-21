e0396457a39f570940e902e4957641dd5d39e1e8
Flavio <flavio.lazzarini@stud.hslu.ch>
2019-10-21 22:52:31 +0200

    added crm unit test project

---
+++ CrmUnitTest/SetContactNameTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CrmUnitTest
+{
+    [TestClass]
+    public class SetContactNameTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
