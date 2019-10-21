73399abb222f15b50ce2d5b4dba3b6285f501192
Flavio <flavio.lazzarini@stud.hslu.ch>
2019-10-21 21:26:21 +0200

    Added SentenceUtils

--- Testing-Tutorial/Program.cs
+++ Testing-Tutorial/Program.cs
@@ -7,6 +7,7 @@ namespace TestingTutorial
         public static void Main(string[] args)
         {
             Console.WriteLine(Hello.HelloMessage());
+            Console.ReadLine();
         }
     }
 }
---
+++ Testing-Tutorial/SentenceUtils.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingTutorial
+{
+    public class SentenceUtils
+    {
+        /// <summary>
+        /// returns a copy of sentence where all the words:
+        /// * start with a capital letter 
+        /// * the rest of the letters are lower case
+        /// </summary>
+        /// <param name="sentence"></param>
+        /// <returns></returns>
+        public static string ToTitleCase(string sentence)
+        {
+            if (sentence == null || sentence.Length == 0)
+            {
+                return sentence;
+            }
+
+            StringBuilder result = new StringBuilder(sentence);
+            result[0] = char.ToUpper(result[0]);
+            for (int i = 1; i < result.Length; i++)
+            {
+                result[i] = char.IsWhiteSpace(result[i - 1]) ? char.ToUpper(result[i]) : char.ToLower(result[i]);
+            }
+
+            return result.ToString();
+        }
+    }
+}
